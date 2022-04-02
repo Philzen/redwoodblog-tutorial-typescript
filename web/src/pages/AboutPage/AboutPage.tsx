d1bdd1879577a572127681b10214ba1f7d65bcba
Philzen <philzen@riseup.net>
2022-04-02 19:28:06 +0200

    Finish tutorial chapter 1: Layouts

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -6,23 +5,10 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site was created to demonstrate my mastery of Redwood: Look on my
-          works, ye mighty, and despair!
-        </p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
     </>
   )
 }
