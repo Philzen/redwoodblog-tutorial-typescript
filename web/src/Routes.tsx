2116b0b0f607c31acdeeaade86c850f92a5faca0
Philzen <philzen@riseup.net>
2022-04-03 01:31:47 +0200

    Finish tutorial chapter 3: Building a form

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -15,6 +15,7 @@ const Routes = () => {
         <Route path="/" page={HomePage} name="home" />
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/contact" page={ContactPage} name="contact" />
       </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
---
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,66 @@
+import { MetaTags } from '@redwoodjs/web'
+import {
+  Form,
+  Submit,
+  Label,
+  TextField,
+  TextAreaField,
+  FieldError,
+} from '@redwoodjs/forms'
+
+const ContactPage = () => {
+  function handleSubmit(
+    data: Record<string, unknown>,
+    e: React.BaseSyntheticEvent
+  ) {
+    console.log(data, e)
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={handleSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          eMail
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
