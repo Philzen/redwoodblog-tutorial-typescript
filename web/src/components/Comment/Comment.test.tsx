0ac982774cac47ead31b3401e20ec8802aedf745
Philzen <philzen@riseup.net>
2022-04-05 23:32:19 +0200

    Finish tutorial chapter 6: Building a Component the Redwood Way

---
+++ web/src/components/Comment/Comment.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@redwoodjs/testing/web'
+import Comment from './Comment'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+
+describe('Comment', () => {
+  it('renders successfully', () => {
+    const comment = {
+      name: 'John Doe',
+      body: 'This is my comment',
+      createdAt: '2020-01-02T12:34:56Z',
+    }
+
+    expect(() => {
+      render(<Comment comment={comment} />)
+    }).not.toThrow()
+
+    expect(screen.getByText(comment.name)).toBeInTheDocument()
+    expect(screen.getByText(comment.body)).toBeInTheDocument()
+
+    const formattedDate = screen.getByText('2 January 2020')
+    expect(formattedDate).toBeInTheDocument()
+    expect(formattedDate.nodeName).toEqual('TIME')
+    expect(formattedDate).toHaveAttribute('datetime', comment.createdAt)
+  })
+})
