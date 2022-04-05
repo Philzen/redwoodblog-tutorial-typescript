a853646e198afd09f101f239de4b5a5588aaaeca
Philzen <philzen@riseup.net>
2022-04-05 23:08:24 +0200

    Improve storybook rendering performance
    - disable processing of (unused) DocGen-Plugin
    - use Storybook V7 style ad-hoc rendering of stories

---
+++ web/config/storybook.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  features: {
+    /** @see {@link https://storybook.js.org/docs/react/configure/overview#on-demand-story-loading} */
+    storyStoreV7: true,
+  },
+  /** @see {@link https://storybook.js.org/docs/react/configure/typescript} */
+  typescript: {
+    reactDocgen: false,
+  },
+}
