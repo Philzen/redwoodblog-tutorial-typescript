b7a59b3ce97786ee6c49103f6b0a05bdb98c937e
Philzen <philzen@riseup.net>
2022-04-06 02:43:06 +0200

    Finish tutorial chapter 5: Our first test

--- web/src/components/Article/Article.test.tsx
+++ web/src/components/Article/Article.test.tsx
@@ -1,21 +1,34 @@
-import { render, screen } from '@redwoodjs/testing'
-
+import { render, screen } from '@redwoodjs/testing/web'
 import Article from './Article'
+
+const ARTICLE = {
+  id: 1,
+  title: 'First post',
+  body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+  createdAt: new Date().toISOString(),
+}
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
 
 describe('Article', () => {
   it('renders a blog post', () => {
-    const article = {
-      id: 1,
-      title: 'First post',
-      body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
-      createdAt: new Date().toISOString(),
-    }
-
     expect(() => {
-      render(<Article article={article} />)
+      render(<Article article={ARTICLE} />)
     }).not.toThrow()
 
-    expect(screen.getByText(article.title)).toBeInTheDocument()
-    expect(screen.getByText(article.body)).toBeInTheDocument()
+    expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
+    expect(screen.getByText(ARTICLE.body)).toBeInTheDocument()
+  })
+
+  it('renders a summary of a blog post', () => {
+    render(<Article article={ARTICLE} summary={true} />)
+
+    expect(screen.getByText(ARTICLE.title)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
+      )
+    ).toBeInTheDocument()
   })
 })
